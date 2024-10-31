a8aae61e14e6308d0a379f3284af8192d2740413
oscar <denfu.oscar.dev@gmail.com>
2024-10-31 16:24:22 +0000

    chore: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,32 @@
-
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Home/Header";
 import Footer from "@/components/Home/Footer";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "MoviesFlix",
-  description: "Your best movie app",
+  title: "mfli-x",
+  description: "Your free movies search app",
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) { 
+}>) {
   return (
     <html lang="en">
-    <head>
-      {/* Link to your favicon */}
-      <link rel="icon" href="/logo.png" type="image/png" />
-    </head>
-    <body className={inter.className}>
-    <Header /> 
-      {children}
-      <Footer />
+      <head>
+        {/* Link to your favicon */}
+        <link rel="icon" href="/logo.png" type="image/png" />
+      </head>
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <Footer />
       </body>
-
-  </html>
+    </html>
   );
 }
